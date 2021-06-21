dc2610719d5f33517d2ee802abf8d20128404aaa
strixen <thomhod@gmail.com>
2021-06-21 12:32:18 +0200

    New version nr

--- core.lua
+++ core.lua
@@ -71,7 +71,7 @@ OnClick = function(self,button,down)
 end,
 OnTooltipShow = function(tooltip) -- Icon tooltip
 	tooltip:AddLine("That's My BIS Tooltips")
-	tooltip:AddLine("Version    : 0.6") -- EDIT TOC and PKMETA
+	tooltip:AddLine("Version    : 0.7") -- EDIT TOC and PKMETA
 	tooltip:AddLine("Left click : Enable/Disable display")
 	tooltip:AddLine("Right click: Open config")
 	tooltip:AddLine("Hold Alt   : Change tooltip display")
